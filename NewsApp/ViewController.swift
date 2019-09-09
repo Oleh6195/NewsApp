21c07d403f2e1d189e700cc641faa229aad54570
Oleh <tyzhai.o@ucu.edu.ua>
2019-09-09 18:51:49 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Олег on 09.09.2019.
+//  Copyright © 2019 OlehTyzhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
